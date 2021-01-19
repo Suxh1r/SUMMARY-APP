82fade9bf4b6ae6fe102d9b331cbf9df96b0ee83
Suxh1r <54749951+Suxh1r@users.noreply.github.com>
2021-01-18 21:30:15 -0600

    Added Functionality to the Summarizer

--- Backend/server/src/app.py
+++ Backend/server/src/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
+from summarizer import Summarizer
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+
+@app.route('/full_summarize/<text>')
+def _summarize(text):
+    model = Summarizer();
+    result = model(text)
+    return result
+
+# @app.route('/hello2')
+# def hello_world2():
+#     return 'Hello World2'
 if __name__ == '__main__':
-    app.run(debug=True,host=)
+    app.run(debug=True)
